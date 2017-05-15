c2bd9ac29db2e27131d590543a5518f473e08a3a
Totoro <arbc139@gmail.com>
2017-05-15 18:40:57 +0900

    Complete glove parser

--- glove-parser/main.py
+++ glove-parser/main.py
@@ -1,4 +1,24 @@
+#!/usr/bin/python
 
-with open('glove.840B.300d.txt', 'r') as glove_file:
-  line = glove_file.readline()
-  print(line)
+import json
+import pymysql
+
+db = pymysql.connect(host='localhost', user='root', password='', db='ai', charset='utf8')
+
+rows = []
+with open('../dataset/glove.840B.300d.txt', 'r') as glove_file:
+  while True:
+    line = glove_file.readline()
+    if not line:
+      break
+    line_arr = line.split()
+    word = line_arr[0]
+    vector = json.dumps(line_arr[1:])
+    rows.append([word, vector])
+
+with db.cursor(pymysql.cursors.DictCursor) as cursor:
+  cursor.executemany(
+    'INSERT INTO GLOVE (name, vector) VALUES (%s, %s)',
+    rows
+  )
+  db.commit()
