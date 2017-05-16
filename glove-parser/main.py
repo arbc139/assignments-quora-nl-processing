e0864447a38a551cc4fe6390e7d2189178a6e1fc
Totoro <arbc139@gmail.com>
2017-05-16 22:09:55 +0900

    Fix tiny bug

--- glove-parser/main.py
+++ glove-parser/main.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python
 
 import json
-import pymysql
+#import pymysql
+import re
+import sys
 
-INPUT_FILE = sys.argv[1]
+def parse_commands(argv):
+  from optparse import OptionParser
+  parser = OptionParser('"')
+  parser.add_option('-i', '--inputFile', dest='input_file', help='Input glove file')
+  parser.add_option('-d', '--dimension', type='int', dest='dimension', help='vector dimension')
+  
+  options, otherjunk = parser.parse_args(argv)
+  return options
 
-db = pymysql.connect(host='localhost', user='root', password='', db='ai', charset='utf8')
+options = parse_commands(sys.argv[1:])
+
+#db = pymysql.connect(host='localhost', user='root', password='', db='ai', charset='utf8')
+
+def is_float(number):
+  try:
+    float(number)
+  except ValueError:
+    return False
+  return True
 
 rows = []
-with open(INPUT_FILE, 'r') as glove_file:
+with open(options.input_file, 'r') as glove_file:
   while True:
     line = glove_file.readline()
     if not line:
       break
     line_arr = line.split()
-    word = line_arr[0]
-    vector = json.dumps(line_arr[1:])
+    word_element_count = len(line_arr) - options.dimension
+    word = ' '.join(line_arr[:word_element_count])
+    vector = json.dumps(line_arr[word_element_count:])
+
+    if len(line_arr[word_element_count:]) != options.dimension:
+      raise RuntimeError('Vector dimension is not equal to parameter')
+
     rows.append([word, vector])
     print('word:', word)
-    break
-
+    print('vector dimension:', len(line_arr[word_element_count:]))
+"""
 with db.cursor(pymysql.cursors.DictCursor) as cursor:
   cursor.executemany(
     'INSERT INTO GLOVE (word, vector) VALUES (%s, %s)',
     rows
   )
   db.commit()
+"""
