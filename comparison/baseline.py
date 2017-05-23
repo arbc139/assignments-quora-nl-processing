338a981da214be3a421efaf35e3a7282b58e2b0c
Totoro <arbc139@gmail.com>
2017-05-23 14:36:45 +0900

    Restart project 1

---
+++ comparison/baseline.py
@@ -0,0 +1,16 @@
+
+from csv_manager import CsvReader, CsvWriter
+
+test_data = None
+with open('dataset/test.csv', 'r') as test_file:
+  csv_reader = CsvReader(test_file)
+  test_data = csv_reader.get_dict_list_data()
+
+with open('dataset/baseline_submission.csv', 'w+') as submission_file:
+  csv_writer = CsvWriter(submission_file, ['test_id', 'is_duplicate'])
+  csv_writer.write_header()
+  for test in test_data:
+    csv_writer.write_row({
+      'test_id': test['test_id'],
+      'is_duplicate': 0,
+    })
