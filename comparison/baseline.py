7c212f3ce6dc68b995369ec02431947052609d77
Totoro <arbc139@gmail.com>
2017-05-27 17:47:17 +0900

    Fix baseline

--- comparison/baseline.py
+++ comparison/baseline.py
@@ -4,11 +4,11 @@ sys.path.append(os.path.abspath(".."))
 from custom_library.csv_manager import CsvReader, CsvWriter
 
 test_data = None
-with open('dataset/test.csv', 'r') as test_file:
+with open('../dataset/input/quora/test.csv', 'r') as test_file:
   csv_reader = CsvReader(test_file)
   test_data = csv_reader.get_dict_list_data()
 
-with open('dataset/baseline_submission.csv', 'w+') as submission_file:
+with open('baseline_submission.csv', 'w+') as submission_file:
   csv_writer = CsvWriter(submission_file, ['test_id', 'is_duplicate'])
   csv_writer.write_header()
   for test in test_data:
