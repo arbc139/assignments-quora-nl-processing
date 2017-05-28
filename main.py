e254115ab36434a7891b38213e9d6ba5e3d1a78f
Totoro <arbc139@gmail.com>
2017-05-29 00:48:50 +0900

    Change parameters

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ params = {
   'objective': 'binary:logistic',
   'eval_metric': 'logloss',
   'eta': 0.02,
-  'max_depth': 10,
+  'max_depth': 15,
   'subsample': 0.6,
   'base_score': 0.3,
 }
@@ -94,7 +94,7 @@ options = {
   'verbose_eval': 50,
 }
 # Number of boosting iterations.
-num_boost_round = 3500
+num_boost_round = 5000
 xgb.train(params, options, num_boost_round)
 y_test = xgb.predict()
 time_logger.log_with_elapse('Step4, Run XGBoost time:')
